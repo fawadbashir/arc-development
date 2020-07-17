4f8ec7e9f073630086518de6e8ea9172a9ed2ae9
Fawad Bashir <bikerz.bashir@gmail.com>
2020-07-17 20:52:33 +0500

    Revert "design course project."
    This reverts commit 3a57ddb1f1cc1e6653ade21e5e07cc02950db6c7.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import App from './components/App';
+import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />
-  ,
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
