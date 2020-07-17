875faa20a544a8e31a32fec1a09feedf02e0141f
Fawad Bashir <bikerz.bashir@gmail.com>
2020-07-17 20:53:00 +0500

    Revert "Revert "design course project.""
    This reverts commit 4f8ec7e9f073630086518de6e8ea9172a9ed2ae9.

---
+++ src/components/ui/theme.js
@@ -0,0 +1,73 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+const arcBlue = '#0B72B9'
+const arcOrange = '#FFBA60'
+const arcGrey = '#868686'
+
+const theme = createMuiTheme({
+  palette: {
+    common: {
+      blue: arcBlue,
+      orange: arcOrange,
+    },
+    primary: {
+      main: arcBlue,
+    },
+    secondary: {
+      main: arcOrange,
+    },
+  },
+  typography: {
+    tab: {
+      fontFamily: 'Raleway',
+      textTransform: 'none',
+      fontSize: '1rem',
+      fontWeight: '700',
+    },
+    estimate: {
+      fontFamily: 'Pacifico',
+      textTransform: 'none',
+      fontSize: '1rem',
+      color: 'white',
+    },
+    h2: {
+      fontFamily: 'Raleway',
+      fontWeight: 700,
+      fontSize: '2.5rem',
+      color: arcBlue,
+      lineHeight: '1.5',
+    },
+    h3: {
+      fontFamily: 'Pacifico',
+      fontSize: '2.5rem',
+      color: arcBlue,
+    },
+    h4: {
+      fontFamily: 'Raleway',
+      fontSize: '1.75rem',
+      color: arcBlue,
+      fontWeight: 700,
+    },
+    subtitle1: {
+      fontSize: '1.25rem',
+      fontWeight: 300,
+      color: arcGrey,
+    },
+    subtitle2: {
+      color: 'white',
+      fontSize: '1.25rem',
+      fontWeight: 300,
+    },
+    learnButton: {
+      borderColor: arcBlue,
+      borderWidth: '2px',
+      textTransform: 'none',
+      color: arcBlue,
+      borderRadius: 50,
+      fontFamily: 'Roboto',
+      fontWeight: 'bold',
+    },
+  },
+})
+
+export default theme
