66398066afffff68f6ded4c4928572738474c296
Fawad Bashir <bikerz.bashi@gmail.com>
2020-09-30 22:46:40 +0500

    updated the contact.js file

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,13 @@
 import React from 'react'
 import { makeStyles, useTheme } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
+
+const useStyles = makeStyles((theme) => ({}))
+
+const Contact = () => {
+	const classes = useStyles()
+
+	return <Grid container></Grid>
+}
+
+export default Contact
