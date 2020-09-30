47d304406cfb14ca5ce96c9cf0d475b2e4902138
Fawad Bashir <bikerz.bashi@gmail.com>
2020-09-30 22:57:50 +0500

    updated the contact.js file

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { makeStyles, useTheme } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+import Typography from '@material-ui/core/Typography'
 
 const useStyles = makeStyles((theme) => ({}))
 
 const Contact = () => {
 	const classes = useStyles()
+	const theme = useTheme()
+	const matchesMD = useMediaQuery(theme.breakpoints.down('md'))
+	const matchesSM = useMediaQuery(theme.breakpoints.down('sm'))
 
 	return <Grid container></Grid>
 }
